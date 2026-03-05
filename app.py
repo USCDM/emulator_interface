b716e78a8aa153f876b9766c14781360092cf0dc
deleaf@github.com <deleaf@users.noreply.github.com>
2026-03-05 11:03:10 -0800

    fixed error in demographics list

--- app.py
+++ app.py
@@ -63,9 +63,9 @@ SUBGROUP_OPTIONS = {
     "Male": "m",
     "Hispanic": "his",
     "Non-Hispanic black": "blk",
-    "Non-Hispanic white": "wht"
+    "Non-Hispanic white": "wht",
     "GED or less than high school": "hsless",
-    "At least some college": "college",
+    "At least some college": "college"
 }
 
 
