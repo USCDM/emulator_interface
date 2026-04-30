eb07d815c6646743f813866c41f32560b4b94e36
deleaf <deleaf@users.noreply.github.com>
2026-04-29 23:13:18 -0700

    added missing string quote

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ OUTCOME_OPTIONS = {
     "PLWD Medicaid costs (annual $)": "t_caidmd_dem",
     "PLWD OOP medical costs (annual $)": "t_oopmd_dem",
     "PLWD QALY loss (annual $)": "qaly_loss_plwd",
-    "PLWD foregone earnings (annual $)": foregone_earnings_plwd"
+    "PLWD foregone earnings (annual $)": "foregone_earnings_plwd"
 }
 
 SUBGROUP_OPTIONS = {
